ff60bd9d9e9e04b016c7cddb2cd8f1c562a4e72c
ChuvilkoDEV <ilyasutton@gmail.com>
2024-07-11 19:26:46 +0300

    Добавлены карточки задач

--- frontend/src/components/Tasks/TaskCard.js
+++ frontend/src/components/Tasks/TaskCard.js
@@ -1,35 +1,93 @@
 // TaskCard.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './TaskCard.css';
+import ImageUtils from '../imageUtils';
+
+const images = ImageUtils.importAllImages(require.context('../../assets/tasks', false, /\.(svg)$/));
+
+const typeLogo = {
+    'subscribers': <img src={images['subscribers.svg']} alt="logo" className='mx-2' />,
+    'reactions': <img src={images['reactions.svg']} alt="logo" className='mx-2' />,
+    'views': <img src={images['views.svg']} alt="logo" className='mx-2' />,
+}
 
 const TaskCard = ({ task }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleToggle = () => {
+        setIsExpanded(!isExpanded);
+    };
+
     return (
-        <div className="task-card">
+        <div className={`task-card ${isExpanded ? 'expanded' : ''}`}>
             <div className="task-card-header">
                 <div className="task-due-date">
+                    <img src={images[`calendarClock.svg`]} alt="logo" />
                     {task.dueDate}
                 </div>
                 <div className="task-title">
-                    {task.title}
+                    <img src={images[`${task.type}.svg`]} alt="logo" />
+                    {task.total}<br />
+                    Просмотров
                 </div>
             </div>
             <div className="task-card-body">
-                <div className="task-status">
-                    {task.status}
+                <div className="status-progress-row">
+                    <div className="task-status">
+                        {task.status}
+                    </div>
+                    <div className="progress-info">
+                        {task.progress}%
+                        {task.progress === 100 ?
+                            <img src={images['success.svg']} alt="logo" />
+                            :
+                            <img src={images['info.svg']} alt="logo" />
+                        }
+                    </div>
                 </div>
                 <div className="task-progress">
                     <div className="progress-bar">
-                        <div className="progress" style={{ width: `${task.progress}%` }}></div>
-                    </div>
-                    <div className="progress-info">
-                        {task.progress}% ({task.completed} из {task.total})
+                        <div
+                            className={`progress ${task.progress === 100 ? 'success' : ''}`}
+                            style={{ width: `${task.progress}%` }}>
+                        </div>
                     </div>
                 </div>
+                <div className="task-statistics">
+                    {task.completed} из {task.total}
+                </div>
             </div>
-            <div className="task-card-footer">
-                <button className="details-button">Подробнее</button>
-            </div>
-        </div>
+            {isExpanded ?
+                <div className="task-card-footer">
+                    <button className="details-button" onClick={handleToggle}>Свернуть</button>
+                    <div className="additional-statistics">
+                        <hr className="divider" />
+                        <div className='additional-statistics title'>
+                            Волна:
+                            <div className="highlight-text"> {task.wave}%</div>
+                        </div>
+                        <div className='additional-statistics title'>
+                            Процент разброса:
+                            <div className="highlight-text"> {task.percent}%</div>
+                        </div>
+                        <div className='additional-statistics title'>
+                            Группа:
+                            <div className="highlight-text"> {task.group}</div>
+                        </div>
+                        <div className='additional-statistics title'>
+                            <div className='calendar'>
+                                <img src={images['calendar.svg']} alt="logo" className='mr-5' />
+                                {task.date}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                :
+                <div className="task-card-footer">
+                    <button className="details-button" onClick={handleToggle}>Подробнее</button>
+                </div>
+            }
+        </div >
     );
 }
 
