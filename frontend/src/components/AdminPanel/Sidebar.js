a0b6c2c9c01ec7de75008abb755fdefa652f0f0e
ChuvilkoDEV <ilyasutton@gmail.com>
2024-07-18 13:10:21 +0300

    Боковая панель админ панели

--- frontend/src/components/AdminPanel/Sidebar.js
+++ frontend/src/components/AdminPanel/Sidebar.js
@@ -2,32 +2,55 @@ import React from 'react';
 import './Sidebar.css';
 
 import ImageUtils from '../imageUtils';
-const images = ImageUtils.importAllImages(require.context('@assets/header', false, /\.(svg)$/));
+const images = ImageUtils.importAllImages(require.context('@assets/admin', false, /\.(svg)$/));
 
-function Sidebar() {
+const menus = [
+  { 'logo': 'accounts', 'title': 'Все аккаунты', 'href': '#' },
+  { 'logo': 'tasks', 'title': 'Все задачи', 'href': '#' },
+  { 'logo': 'support', 'title': 'Тех. поддержка', 'href': '#' },
+]
+
+const SidebarHeader = () => (
+  <div className="sidebar-header">
+    <div className="sidebar-header-container">
+      <h1 className="sidebar-header-title">
+        T
+        <img src={images['telegram.svg']} alt="logo" className='sidebar-header-logo' />
+        UP
+      </h1>
+    </div>
+    <hr />
+  </div>
+);
+
+const MenuConstructor = (menuItem) => (
+  <li key={menuItem.logo}>
+    <a href={menuItem.href}>
+      <img src={images[`${menuItem.logo}.svg`]} alt="logo" />
+      {menuItem.title}
+    </a>
+  </li>
+);
+
+const SidebarMenu = () => (
+  <ul className="sidebar-menu">
+    {menus.map(MenuConstructor)}
+  </ul>
+)
+
+const SidebarFooter = () => (
+  <div className="sidebar-footer">
+    <button>Добавить задачу</button>
+    <button>Добавить сессию</button>
+  </div>
+)
+
+export default function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar-header">
-        <div className="sidebar-header-container">
-          <h1 className="sidebar-header-title">
-            T
-            <img src={images['telegram.svg']} alt="logo" className='sidebar-header-logo' />
-            UP
-          </h1>
-        </div>
-        <hr />
-      </div>
-      <ul className="sidebar-menu">
-        <li><a href="#">Все аккаунты</a></li>
-        <li><a href="#">Управление аккаунтами</a></li>
-        <li><a href="#">Все задачи</a></li>
-      </ul>
-      <div className="sidebar-footer">
-        <button>Добавить задачу</button>
-        <button>Добавить сессию</button>
-      </div>
+      <SidebarHeader />
+      <SidebarMenu />
+      <SidebarFooter />
     </div>
   );
 }
-
-export default Sidebar;
