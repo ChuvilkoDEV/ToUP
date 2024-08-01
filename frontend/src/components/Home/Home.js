0ec2bf7d59fa423970f469a872bf6fdfc977237b
ChuvilkoDEV <ilyasutton@gmail.com>
2024-08-01 23:54:57 +0300

    Переместил подвал в низ страницы

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
-
+import './Home.css'
 const Home = () => {
   return (
     <>
       <Header />
-      <div>
-        {/* <h1>Главная страница</h1>
-        <p>Добро пожаловать на главную страницу!</p> */}
+      <div className='home'>
+        <h1>Главная страница</h1>
+        <p>Добро пожаловать на главную страницу!</p>
       </div>
       <Footer />
     </>
